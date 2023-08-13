e668006c1aa7c960834276561957ed0cf1ff3107
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 13:44:52 -0300

    Feat: homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Cadastro from './Cadastro';
+import Home from './Home';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/home" element={<Home/>} />
         </Routes>
       </div>
     </Router>
---
+++ src/Home.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [photo, setPhoto] = useState('');
+    const [category, setCategory] = useState('');
+    const [status, setStatus] = useState('');
+    const [price, setPrice] = useState('');
+    const [phoneSeller, setPhoneSeller] = useState('');
+  
+    const url = 'http://localhost:4000';
+  
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(url + '/home');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+  
+    const handleCreateProduct = async (e) => {
+      e.preventDefault();
+  
+      try {
+        await axios.post(url + '/products', {
+          name,
+          description,
+          photo,
+          category,
+          status,
+          price,
+          phone_seller: phoneSeller
+        });
+  
+        fetchProducts();
+  
+        setName('');
+        setDescription('');
+        setPhoto('');
+        setCategory('');
+        setStatus('');
+        setPrice('');
+        setPhoneSeller('');
+        setShowForm(false); // Fechar o formulário após a criação
+      } catch (error) {
+        console.error('Erro ao criar produto:', error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Produtos</h1>
+        <div>
+          <h2>Lista de Produtos</h2>
+          <ul>
+            {products.map(product => (
+              <li key={product.id}>
+                <div>
+                  <img src={product.photo} alt={product.name} />
+                </div>
+                <div>
+                  <strong>{product.name}</strong>
+                  <p>{product.description}</p>
+                  <p>Preço: R$ {product.price}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2>Criar Novo Produto</h2>
+          {showForm ? (
+                <form onSubmit={handleCreateProduct}>
+                <label>Nome:</label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br />
+
+                <label>Descrição:</label>
+                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required /><br />
+
+                <label>Foto:</label>
+                <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} required /><br />
+
+                <label>Categoria:</label>
+                <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} required /><br />
+
+                <label>Status:</label>
+                <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} required /><br />
+
+                <label>Preço:</label>
+                <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required /><br />
+
+                <label>Telefone do Vendedor:</label>
+                <input type="text" value={phoneSeller} onChange={(e) => setPhoneSeller(e.target.value)} required /><br />
+
+                <button type="submit">Criar Produto</button>
+            </form>
+          ) : (
+            <button onClick={() => setShowForm(true)}>Novo Produto</button>
+          )}
+        </div>
+      </div>
+    );
+};
+
+
+export default Home;
