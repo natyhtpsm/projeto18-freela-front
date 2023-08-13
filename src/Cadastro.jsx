d07162061a418ae42084781222b6fcb9a3cfc419
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 13:20:40 -0300

    Feat: handle cadastro handle login

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Cadastro = () => {
-  const [nome, setNome] = useState('');
+  const [name, setNome] = useState('');
   const [cpf, setCpf] = useState('');
-  const [telefone, setTelefone] = useState('');
+  const [phone, setTelefone] = useState('');
   const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-
-  const handleCadastro = (e) => {
+  const [password, setSenha] = useState('');
+  const url =  'http://localhost:4000';
+  const handleCadastro = async (e) => {
     e.preventDefault();
-    // Aqui você deve adicionar a lógica para cadastrar o usuário
+    try {
+      const response = await axios.post(url + '/cadastro', {
+        name,
+        phone,
+        email,
+        password
+      });
+      console.log('Usuário cadastrado:', response.data);
+    } catch (error) {
+      console.error('Erro ao cadastrar usuário:', error);
+    }
   };
 
   return (
@@ -17,21 +28,21 @@ const Cadastro = () => {
       <h1>Cadastro</h1>
       <form onSubmit={handleCadastro}>
         <label>Nome:</label>
-        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required /><br />
+        <input type="text" value={name} onChange={(e) => setNome(e.target.value)} required /><br />
 
         <label>CPF:</label>
         <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} required /><br />
 
         <label>Telefone:</label>
-        <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} required /><br />
+        <input type="text" value={phone} onChange={(e) => setTelefone(e.target.value)} required /><br />
 
         <label>E-mail:</label>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required /><br />
 
         <label>Senha:</label>
-        <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required /><br />
+        <input type="password" value={password} onChange={(e) => setSenha(e.target.value)} required /><br />
 
-        <button type="submit">Cadastrar</button>
+        <button type="submit" onClick={handleCadastro}>Cadastrar</button>
       </form>
     </div>
   );
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  
+  const navigate = useNavigate()
+  const url = 'http://localhost:4000';
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
+
+    try {
+      const response = await axios.post(url + '/', {
+        email,
+        password: senha
+      });
+
+      console.log('Login bem-sucedido:', response.data);
+      navigate('/home');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
   };
 
   return (
@@ -18,7 +35,7 @@ const Login = () => {
         <label>Senha:</label>
         <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required /><br />
         
-        <button type="submit">Entrar</button>
+        <button type="submit" onClick={handleLogin}>Entrar</button>
       </form>
       <p>Ainda não tem uma conta? <a href="/cadastro">Cadastre-se</a></p>
     </div>
