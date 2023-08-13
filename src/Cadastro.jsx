e910cfe844a62a6584fd0a84e474f954f2a52057
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 19:28:30 -0300

    Style: cadastro

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const Cadastro = () => {
   const [name, setNome] = useState('');
@@ -8,6 +9,7 @@ const Cadastro = () => {
   const [email, setEmail] = useState('');
   const [password, setSenha] = useState('');
   const url =  'http://localhost:4000';
+
   const handleCadastro = async (e) => {
     e.preventDefault();
     try {
@@ -24,28 +26,75 @@ const Cadastro = () => {
   };
 
   return (
-    <div>
+    <CadastroContainer>
       <h1>Cadastro</h1>
-      <form onSubmit={handleCadastro}>
+      <CadastroForm onSubmit={handleCadastro}>
         <label>Nome:</label>
-        <input type="text" value={name} onChange={(e) => setNome(e.target.value)} required /><br />
-
+        <input type="text" value={name} onChange={(e) => setNome(e.target.value)} required />
+        
         <label>CPF:</label>
-        <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} required /><br />
-
+        <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} required />
+        
         <label>Telefone:</label>
-        <input type="text" value={phone} onChange={(e) => setTelefone(e.target.value)} required /><br />
-
+        <input type="text" value={phone} onChange={(e) => setTelefone(e.target.value)} required />
+        
         <label>E-mail:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required /><br />
-
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        
         <label>Senha:</label>
-        <input type="password" value={password} onChange={(e) => setSenha(e.target.value)} required /><br />
-
-        <button type="submit" onClick={handleCadastro}>Cadastrar</button>
-      </form>
-    </div>
+        <input type="password" value={password} onChange={(e) => setSenha(e.target.value)} required />
+        
+        <button type="submit">Cadastrar</button>
+      </CadastroForm>
+    </CadastroContainer>
   );
 };
 
 export default Cadastro;
+
+
+const CadastroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f2f2f2;
+`;
+
+const CadastroForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+
+  label {
+    margin-bottom: 8px;
+    font-weight: bold;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  button {
+    padding: 8px 16px;
+    background-color: #ff6f61;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #ff4f3b;
+    }
+  }
+`;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,118 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [showForm, setShowForm] = useState(false);
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [photo, setPhoto] = useState('');
-    const [category, setCategory] = useState('');
-    const [status, setStatus] = useState('');
-    const [price, setPrice] = useState('');
-    const [phoneSeller, setPhoneSeller] = useState('');
+  const [products, setProducts] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [photo, setPhoto] = useState('');
+  const [category, setCategory] = useState('');
+  const [status, setStatus] = useState('');
+  const [price, setPrice] = useState('');
 
-    const url = 'http://localhost:4000';
+  const url = 'http://localhost:4000';
 
-    const fetchProducts = async () => {
-        try {
-            const response = await axios.get(url + '/home');
-            setProducts(response.data);
-        } catch (error) {
-            console.error('Erro ao buscar produtos:', error);
-        }
-    };
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(url + '/home');
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar produtos:', error);
+    }
+  };
 
-    const handleCreateProduct = async (e) => {
-        e.preventDefault();
+  const handleCreateProduct = async (e) => {
+    e.preventDefault();
 
-        try {
-            await axios.post(url + '/produto', {
-                name,
-                description,
-                photo,
-                category,
-                status,
-                price,
-                phone_seller: phoneSeller
-            });
+    try {
+      await axios.post(url + '/produto', {
+        name,
+        description,
+        photo,
+        category,
+        status,
+        price
+      });
 
-            fetchProducts();
+      fetchProducts();
+      setName('');
+      setDescription('');
+      setPhoto('');
+      setCategory('');
+      setStatus('');
+      setPrice('');
+      setShowForm(false);
+    } catch (error) {
+      console.error('Erro ao criar produto:', error);
+    }
+  };
 
-            setName('');
-            setDescription('');
-            setPhoto('');
-            setCategory('');
-            setStatus('');
-            setPrice('');
-            setPhoneSeller('');
-            setShowForm(false); 
-        } catch (error) {
-            console.error('Erro ao criar produto:', error);
-        }
-    };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <div>
+        <h2>Lista de Produtos</h2>
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              <Link to={`/produto/${product.id}`}>
+                <div>
+                  <img src={product.photo} alt={product.name} />
+                </div>
+                <div>
+                  <strong>{product.name}</strong>
+                  <p>{product.description}</p>
+                  <p>Preço: R$ {product.price}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Criar Novo Produto</h2>
+        {showForm ? (
+          <form onSubmit={handleCreateProduct}>
+            <label>Nome:</label>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br />
 
-    return (
-        <div>
-            <h1>Produtos</h1>
-            <div>
-                <h2>Lista de Produtos</h2>
-                <ul>
-                    {products.map(product => (
-                        <li key={product.id}>
-                            <Link to={`/produto/${product.id}`}>
-                                <div>
-                                    <img src={product.photo} alt={product.name} />
-                                </div>
-                                <div>
-                                    <strong>{product.name}</strong>
-                                    <p>{product.description}</p>
-                                    <p>Preço: R$ {product.price}</p>
-                                </div>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <h2>Criar Novo Produto</h2>
-                {showForm ? (
-                    <form onSubmit={handleCreateProduct}>
-                        <label>Nome:</label>
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br />
+            <label>Descrição:</label>
+            <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required /><br />
 
-                        <label>Descrição:</label>
-                        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required /><br />
+            <label>Foto:</label>
+            <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} required /><br />
 
-                        <label>Foto:</label>
-                        <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} required /><br />
+            <label>Categoria:</label>
+            <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} required /><br />
 
-                        <label>Categoria:</label>
-                        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} required /><br />
+            <label>Status:</label>
+            <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} required /><br />
 
-                        <label>Status:</label>
-                        <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} required /><br />
+            <label>Preço:</label>
+            <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required /><br />
 
-                        <label>Preço:</label>
-                        <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required /><br />
-
-                        <label>Telefone do Vendedor:</label>
-                        <input type="text" value={phoneSeller} onChange={(e) => setPhoneSeller(e.target.value)} required /><br />
-
-                        <button type="submit" onClick={handleCreateProduct}>Criar Produto</button>
-                    </form>
-                ) : (
-                    <button onClick={() => setShowForm(true)}>Novo Produto</button>
-                )}
-            </div>
-        </div>
-    );
+            <button type="submit">Criar Produto</button>
+          </form>
+        ) : (
+          <button onClick={() => setShowForm(true)}>Novo Produto</button>
+        )}
+      </div>
+    </div>
+  );
 };
-
 
 export default Home;
