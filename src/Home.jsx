f403121d7d05cbee973efa7eb27d57ae68c7e3d5
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 21:41:47 -0300

    Style: Homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { validateUser } from './Validate';
 import AuthContext from './Context';
 import { headersAuth } from './Autenticacao';
+import styled from 'styled-components';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -15,7 +16,6 @@ const Home = () => {
   const [status, setStatus] = useState('');
   const [price, setPrice] = useState('');
   const { user, setUser } = useContext(AuthContext);
-
 
   const url = 'http://localhost:4000';
 
@@ -64,31 +64,29 @@ const Home = () => {
   }, [user]);
 
   return (
-    <div>
+    <HomeContainer>
       <h1>Produtos</h1>
-      <div>
+      <ProductList>
         <h2>Lista de Produtos</h2>
         <ul>
           {products.map(product => (
-            <li key={product.id}>
+            <ProductItem key={product.id}>
               <Link to={`/produto/${product.id}`}>
-                <div>
-                  <img src={product.photo} alt={product.name} />
-                </div>
+                <ProductImage src={product.photo} alt={product.name} />
                 <div>
                   <strong>{product.name}</strong>
                   <p>{product.description}</p>
                   <p>Preço: R$ {product.price}</p>
                 </div>
               </Link>
-            </li>
+            </ProductItem>
           ))}
         </ul>
-      </div>
+      </ProductList>
       <div>
         <h2>Criar Novo Produto</h2>
         {showForm ? (
-          <form onSubmit={handleCreateProduct}>
+          <CreateProductForm onSubmit={handleCreateProduct}>
             <label>Nome:</label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br />
 
@@ -107,14 +105,91 @@ const Home = () => {
             <label>Preço:</label>
             <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} required /><br />
 
-            <button type="submit">Criar Produto</button>
-          </form>
+            <CreateProductButton type="submit">Criar Produto</CreateProductButton>
+          </CreateProductForm>
         ) : (
           <button onClick={() => setShowForm(true)}>Novo Produto</button>
         )}
       </div>
-    </div>
+    </HomeContainer>
   );
 };
 
 export default Home;
+
+const HomeContainer = styled.div`
+  text-align: center;
+  background-color: #f2f2f2;
+  padding: 20px;
+  min-height: 100vh;
+`;
+
+const ProductList = styled.div`
+  margin-top: 20px;
+`;
+
+const ProductItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  background-color: white;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+`;
+
+const ProductImage = styled.img`
+  width: 100px;
+  height: 100px;
+  margin-right: 10px;
+`;
+
+const CreateProductForm = styled.form`
+  margin-top: 20px;
+  text-align: left;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+
+  label {
+    margin-bottom: 8px;
+    font-weight: bold;
+  }
+
+  input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  button {
+    padding: 8px 16px;
+    background-color: #ff6f61;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #ff4f3b;
+    }
+  }
+`;
+
+const CreateProductButton = styled.button`
+  background-color: #ff6f61;
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #ff4f3b;
+  }
+`;
